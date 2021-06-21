082dfae7559cd705198cbaf4fc622523a7f44e57
xialin <499300201@qq.com>
2021-06-21 20:28:33 +0800

    添加maven_01至git

---
+++ maven_01/src/main/java/Demo1.java
@@ -0,0 +1,10 @@
+/**
+ * @author xialin
+ * @date 2021/6/18 22:05
+ * @description
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("maven创建的Java项目");
+    }
+}
---
+++ maven_01/src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author xialin
+ * @date 2021/6/18 22:09
+ * @description
+ */
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("执行测试");
+    }
+}
